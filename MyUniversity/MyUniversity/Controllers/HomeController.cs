15f92d2ff73271b88a27409c066740d4d9cb4e47
Ehsan <ehsan.mehralian@gmail.com>
2017-07-17 15:09:53 +0430

    find number of enrollments in each date and show in the about

--- MyUniversity/MyUniversity/Controllers/HomeController.cs
+++ MyUniversity/MyUniversity/Controllers/HomeController.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyUniversity.Data;
+using MyUniversity.Models.SchoolViewModels;
 
 namespace MyUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext contex)
+        {
+            _context = contex;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
---
+++ MyUniversity/MyUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyUniversity.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
